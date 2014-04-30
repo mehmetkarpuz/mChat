8d00c22e91195f4e33e9978e725821d8fb1f6df4
mehmetkarpuz <memetkarpuz@gmail.com>
2014-04-30 17:20:04 +0300

    ree

--- mChatIceApp/Global.asax.cs
+++ mChatIceApp/Global.asax.cs
@@ -17,11 +17,9 @@ namespace mChatIceApp
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
     }
 }
