2a76d6fb0a0e5300b6d175dda9ec7565f370ec96
mehmetkarpuz <memetkarpuz@gmail.com>
2014-04-30 12:15:40 +0300

    mChatIceApp
    Chat App Third Commit

---
+++ mChatIceApp/Controllers/mChatController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace mChatIceApp.Controllers
+{
+    public class mChatController : ApiController
+    {
+        [AcceptVerbs("GET", "POST")]
+        public HttpResponseMessage ConnectUser(string username)
+        {
+            HttpContext.Current.AcceptWebSocketRequest(new ChatSocketHandler(username));
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+    }
+}
